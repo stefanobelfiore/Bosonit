2002539fa2b0c6c73825980fa28b6bb40b7fb586
Stefano Belfiore <stefano.belfiore@bosonit.com>
2021-11-12 16:59:35 +0100

    ejercicio 2 terminado

---
+++ demo/src/main/java/com/example/demo/Controler.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class Controler {
+
+    @GetMapping("/hola")
+    public String getHolaMundo(){
+        return "Hola Mundo";
+    }
+
+    @GetMapping("user/{nombre}")
+    public String getEmployeesById(@PathVariable String nombre) {
+        return "Hola: " + nombre;
+    }
+
+    @PostMapping("/useradd")
+    public User printUser(@RequestBody User user){
+        User answer = new User();
+        answer.setName(user.getName());
+        answer.setEdad(user.getEdad());
+        answer.setCiudad(user.getCiudad());
+        return answer;
+    }
+
+}
---
+++ demo/src/main/java/com/example/demo/User.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+public class User {
+
+    private String name;
+    private String edad;
+    private String ciudad;
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+}
