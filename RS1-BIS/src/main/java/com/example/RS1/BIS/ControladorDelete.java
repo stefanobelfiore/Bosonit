bea67050a607a954442a59f22b35763ded212d60
stefano <78228898+stefanobelfiore@users.noreply.github.com>
2021-11-23 19:02:03 +0100

    Update ControladorDelete.java

--- RS1-BIS/src/main/java/com/example/RS1/BIS/ControladorDelete.java
+++ RS1-BIS/src/main/java/com/example/RS1/BIS/ControladorDelete.java
@@ -4,9 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.stream.Stream;
+import java.util.Iterator;
 
 @SuppressWarnings("SpellCheckingInspection")
 @RestController
@@ -16,12 +14,15 @@ public class ControladorDelete {
     PersonList personList;
 
     @DeleteMapping("persona/{id}")
-    public List deletePerson(@PathVariable int id){
-
-        personList.person.remove(id);
-        
-
-        return personList.person;
-
+    public String deletePerson(@PathVariable int id){
+        Iterator<PersonInterface> iter = personList.person.iterator();
+        for(PersonInterface person: personList.person) {
+            if(person.getId() == id){
+                personList.person.remove(person);
+                System.out.println(person.getId());
+            }
+        }
+        return "La persona con el id "+ id + "ha sido eliminada";
     }
+    
 }
